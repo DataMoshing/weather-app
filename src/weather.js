49ef5ad4f658607eef72eafacc585b6a6722594c
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-04-24 19:33:24 -0700

    Wrapped logic in an async function

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,14 @@
-const URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=02dbfb3b0a9eca931937cb67ff5eba51"
+async function getApi() {
 
-const response = await fetch(URL, { mode: "cors" })
-const getData = await response.json()
-
-export default getData
+    const URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=02dbfb3b0a9eca931937cb67ff5eba51&units=imperial"
+    try {
+        const response = await fetch(URL, { mode: 'cors' })
+        const getData = await response.json()
+        console.log(getData)
+        return getData
+    } catch (error) {
+        console.log(error)
+        return undefined
+    }
+}
+export default getApi
