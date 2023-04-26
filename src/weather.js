a689419e2fa07469700b62c887add801c9b18719
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-04-25 22:49:53 -0700

    Refactored logic to use the Geocoding API

--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("generate")
 
 async function getApi() {
     try {
-        let URL = "http://api.openweathermap.org/geo/1.0/direct?&limit=5&appid=02dbfb3b0a9eca931937cb67ff5eba51&units=imperial&q="
+        let URL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=02dbfb3b0a9eca931937cb67ff5eba51&q="
         const input = document.getElementById("search").value.trim()
         URL = URL.concat(input)
         const response = await fetch(URL, { mode: "cors" })
