8707cdcfd5ffa9a488561a1e1ef5bae871e19246
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-04-28 17:56:28 -0700

    Can display weather data onto web page

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,4 @@
 const btn = document.getElementById("generate")
-
 async function getApi() {
     try {
         let URL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=02dbfb3b0a9eca931937cb67ff5eba51&q="
@@ -8,9 +7,14 @@ async function getApi() {
         const response = await fetch(URL, { mode: "cors" })
         const getData = await response.json()
         console.log(getData)
+        const mainStr = [getData.sys.country, getData.name, getData.main, getData.weather[0].description]
+        let serializedStr = JSON.stringify(mainStr, null, " ")
+        serializedStr = serializedStr.replace(/["']/g, "").replace(/[{}]/g, "").replace(/[[\]]/g, "")
+        document.body.append(serializedStr)
         return getData
     } catch (error) {
         console.log(error)
+        alert("Location not found, please try again.")
         return undefined
     }
 }
