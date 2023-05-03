378402e2f4a61b79e17e670c65f9f411bd944930
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-05-03 12:58:30 -0700

    Imported and called displayClock function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { btn, getApi } from "./weather"
 import { display, weatherMain, locationDiv } from "./dom"
+import displayClock from "./time";
 
 btn.addEventListener("click", (e) => {
     e.preventDefault()
@@ -8,3 +9,5 @@ btn.addEventListener("click", (e) => {
     weatherMain.textContent = ""
     locationDiv.textContent = ""
 })
+
+displayClock()
