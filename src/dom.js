412170b591ff61bcd52b48605f044eb82d54fbc7
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-05-02 18:21:53 -0700

    Added dom manipulation to elements

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,48 @@
-// import { getApi } from "./weather";
-
 function display(getData) {
-    const mainStr = [getData.sys.country, getData.name, getData.main, getData.weather[0].description]
+    console.log(getData)
+    const mainWrap = document.createElement("div")
+    const weatherMain = document.createElement("div")
+    const locationDiv = document.createElement("div")
+    const descriptionDiv = document.createElement("div")
+    const temperature = document.createElement("p")
+    const feelsLike = document.createElement("p")
+    const humidity = document.createElement("p")
+    const highTemp = document.createElement("p")
+    const lowTemp = document.createElement("p")
 
-    let serializedObj = JSON.stringify(mainStr, null, " ")
-    serializedObj = serializedObj.replace(/["']/g, "").replace(/[{}]/g, "").replace(/[[\]]/g, "")
+    mainWrap.className = "main-wrap"
+    weatherMain.className = "weather-main"
+    locationDiv.className = "location"
+    descriptionDiv.className = "description"
+    temperature.className = "temperature"
+    feelsLike.className = "feels-like"
+    humidity.className = "humidity"
+    highTemp.className = "high"
+    lowTemp.className = "low"
 
-    document.body.append(serializedObj)
+
+    const location = [getData.sys.country, getData.name]
+    const temp = `Temperature: ${getData.main.temp} °F`
+    const feelsLikeTemp = `Feels like: ${getData.main.feels_like} °F`
+    const humidityTemp = `Humidity: ${getData.main.humidity}%`
+    const high = `High: ${getData.main.temp_max} °F`
+    const low = `Low: ${getData.main.temp_min} °F`
+    const description = [getData.weather[0].description]
+    const mainStr = [location, temp, feelsLikeTemp, humidityTemp, high, low, description]
+
+    const serializedObj = JSON.stringify(mainStr, null, " ")
+
+    locationDiv.append(location)
+    temperature.append(temp)
+    feelsLike.append(feelsLikeTemp)
+    humidity.append(humidityTemp)
+    highTemp.append(high)
+    lowTemp.append(low)
+    descriptionDiv.append(description)
+    mainWrap.append(weatherMain)
+    weatherMain.append(locationDiv, temperature, feelsLike, humidity, highTemp, lowTemp, descriptionDiv)
+    document.body.append(mainWrap)
+    return serializedObj
 }
 
 export default display
